5141f2e6e6965d15205090cb9fe2d3df9d16bb53
unknown <ajmalmahir@icloud.com>
2025-08-12 00:00:00 +0100

    moved sudoku to it's own file

--- src/App.js
+++ src/App.js
@@ -1,56 +1,10 @@
 import './App.css';
-import { useState } from 'react';
-
-import Cell from './Cell'
-import { getCellMates, createGrid } from './utils';
+import Sudoku from './Sudoku';
 import { sudokuPuzzles } from './sudokuPuzzles';
+import { createGrid } from './utils';
 
 const selectedPuzzle = sudokuPuzzles.easy[0].puzzle;
 const grid = createGrid(selectedPuzzle)
-
-const Sudoku = (props) => {
-  const [grid, setGrid] = useState(props.grid)
-
-  const highlightMates = (cellMates) => {
-    setGrid(
-      grid.map((cell) => {
-        if (cellMates.includes(cell.id)) {
-          return {
-            ...cell,
-            isHighlighted: true,
-          }
-        }
-        return {
-          ...cell,
-          isHighlighted: false
-        }
-      })
-    )
-  }
-
-  const clearHighlights = () => {
-    setGrid(
-      grid.map((cell) => ({
-        ...cell,
-        isHighlighted: false
-      }))
-    )
-  }
-
-  return (
-    <div className='sudoku'>
-      {grid.map((cell) => (
-        <Cell 
-          key={cell.id} 
-          highlightMates={highlightMates}
-          clearHighlights={clearHighlights}
-          cellMates={getCellMates(grid, cell)}
-          {...cell} 
-        />
-      ))}
-    </div>
-  )
-}
 
 function App() {
   return (
